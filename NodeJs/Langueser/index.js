17e2d3cc34e6dcf8f476080764eef32e7963fdb0
MarteMuro <115433630+MarteMuro@users.noreply.github.com>
2023-08-03 11:32:21 -0600

    node continued

---
+++ NodeJs/Langueser/index.js
@@ -0,0 +1,3 @@
+const franc = require("franc");
+
+console.log(franc('Alle menslike wesens word vry') )//=> 'afr'
--- NodeJs/jocker/index.js
+++ NodeJs/jocker/index.js
@@ -1,3 +1,18 @@
-const jokes = require("give-me-a-joke")
 
-console.log(jokes);
+
+//const jokes = require("give-me-a-joke");
+
+// To get a random Chuck Norris joke
+//jokes.getRandomCNJoke(function(joke) {
+   
+   // => console.log(joke);
+//}) ;   esto no funciono
+
+// segundo intento con pacg colors
+
+var colors = require("colors");
+
+console.log("hello".red);
+console.log("hello".underline.red);
+console.log("hello whit rainbow colors".rainbow);
+console.log("hello".trap);
