65fb09b1d598f7380f732b83bea4963ed066252f
MarteMuro <115433630+MarteMuro@users.noreply.github.com>
2023-08-04 12:48:51 -0600

    into express

--- express/firstApp/index.js
+++ express/firstApp/index.js
@@ -1,3 +1,38 @@
 const express = require('express');
 const app = express();
-console.dir(app);
+
+// este es el prmer ejemplo para mandar info  desde express
+/* app.use((req,res) => {
+    console.log("we got a new request")
+    res.send('C:\Users\mmuro\OneDrive - Joyson Group\Documents\web bootcamp udemy\express\firstApp\main.html')
+}) */
+// ++++++++++++aqui empezare otro ejemplo leccion 343 udemy
+app.get('/R/:subreddit', (req,res) => {
+    res.send('This is a subreddi!')
+    let result = req.params;
+    console.log(result);
+
+})
+app.get('/search?',(req,res,) => {
+    console.log(req.query)
+    const { q } = req.query;
+    if(!q) {
+        res.send('nothing to search')
+    }
+    res.send(`<h1>Search result for ${q}<h1>`)
+    
+})
+
+app.get('/',(req,res) => {
+    res.send('This is the Home page')
+})
+app.get('/cats',(req,res) => {
+      res.send('Meow!!')
+})
+
+
+
+app.listen(3000, () => {
+    console.log(`Example app listening on por 3000`)
+})
+
