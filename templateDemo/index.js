b5544b6d302caf612aa91ead9f60bac286ac0183
MarteMuro <115433630+MarteMuro@users.noreply.github.com>
2023-08-07 09:55:01 -0600

    using ejs

---
+++ templateDemo/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/' , (req,res) => {
+    res.send("hi")
+})
+
+app.listen(3000, () => {
+    console.log("listeing on port 3000")
+})
