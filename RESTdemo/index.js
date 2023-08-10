7826837825edce5ed2640ce1d2834a0bb959b56a
MarteMuro <115433630+MarteMuro@users.noreply.github.com>
2023-08-10 16:54:49 -0600

    RESTDemo working good!

--- RESTdemo/index.js
+++ RESTdemo/index.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
+const { v4: uuid } = require('uuid');
 
 
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded is middleware to pars the data from form
 app.use(express.json()) // to recived data in JSON form
 // con esto comandos usas la carpeta de views donde guardas las paginas ejs
 // tambien usas la const path 
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs' )
 
 // esto hzio un array de coments 
-const comments = [
+let comments = [
     {
+        id:uuid(),
         username:'Todd',
         comment: 'Lol that is so funny!'
     },
     {
+        id:uuid(),
         username:'Skyler',
         comment: 'I like booms!'
     },
     {
+        id:uuid(),
         username:'Sk8terB0i',
         comment: 'Plz delete u acct Todd!'
     },
     {
+        id:uuid(),
         username:'Onlysayswoo',
         comment: 'woof wooof!'
     }
@@ -42,8 +49,33 @@ app.get('/comments/new', (req,res) => {
 } )
 app.post('/comments',(req,res)  => {
     const { username, comment} = req.body;
-    comments.push({username, comment});
+    comments.push({username, comment, id:uuid() });
     res.redirect('/comments');
+})
+app.patch('/comments/:id', (req,res)  => {
+    const { id} =req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find( c => c.id === id );
+    foundComment.comment = newCommentText;
+   res.redirect('/comments')
+} )
+app.delete('/comments/:id' ,(req,res)  => {
+    const { id} =req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments')
+    
+})
+
+app.get('/comments/:id', (req,res)  => {
+    const { id} =req.params;
+    const comment =comments.find( c => c.id === id );
+    res.render('comments/show' , {comment});
+})
+
+app.get('/comments/:id/edit', (req,res) => {
+    const { id} =req.params;
+    const comment =comments.find( c => c.id === id );
+    res.render('comments/edit' , {comment})
 })
 
 app.get('/tacos', (req,res) => {
